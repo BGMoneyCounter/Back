162ea13b1e599832ddd95833e9f298a0ada3a5ba
pomionot <Kaza.Harusame@gmail.com>
2025-06-28 19:01:06 +0900

    editEntity

--- myapp/src/main/java/com/example/myapp/entity/Games.java
+++ myapp/src/main/java/com/example/myapp/entity/Games.java
@@ -1,30 +1,31 @@
-//package com.example.myapp.entity;
-//import jakarta.persistence.Column;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.JoinColumn;
-//import jakarta.persistence.ManyToOne;
-//import jakarta.persistence.OneToMany;
-//import jakarta.persistence.Table;
-//import lombok.Getter;
-//import lombok.Setter;
-//@Entity
-//@Table(name="games")
-//@Getter
-//@Setter
-//public class Games {
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	private int game_id;
-//	
-//	@Column(nullable = false)
-//	private String game_name;
-//	
-//	@Column(nullable= false)
-//	private String pass;
-//	
-//	@Column(nullable= false)
-//	private boolean end_flag;
-//}
+package com.example.myapp.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+@Entity
+@Table(name="games")
+@Getter
+@Setter
+public class Games {
+	@Id
+	@GneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "game_id")
+    private int gameId;
+
+    @Column(name = "game_name", nullable = false)
+    private String gameName;
+
+    @Column(nullable = false)
+    private String pass;
+
+    @Column(name = "end_flag", nullable = false)
+    private boolean endFlag;
+}
--- myapp/src/main/java/com/example/myapp/entity/Players.java
+++ myapp/src/main/java/com/example/myapp/entity/Players.java
@@ -1,31 +1,32 @@
-//package com.example.myapp.entity;
-//import jakarta.persistence.Column;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.JoinColumn;
-//import jakarta.persistence.ManyToOne;
-//import jakarta.persistence.OneToMany;
-//import jakarta.persistence.Table;
-//import lombok.Getter;
-//import lombok.Setter;
-//@Entity
-//@Table(name="players")
-//@Getter
-//@Setter
-//public class Players {
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	private int player_id;
-//	
-//	@Column(nullable = false)
-//	private boolean gm_flag;
-//	
-//	@Column()
-//	private int money
-//	
-//	@ManyToOne
-//    @JoinColumn(name = "games", referencedColumnName = "game_id", nullable = false)
-//	private Games game_id;
-//}
+package com.example.myapp.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+@Entity
+@Table(name="players")
+@Getter
+@Setter
+public class Players {
+	@Id
+	@GneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "player_id")
+    private int playerId;
+
+    @Column(name = "gm_flag", nullable = false)
+    private boolean gmFlag;
+
+    @Column(nullable = false)
+    private int money;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id", referencedColumnName = "game_id", nullable = false)
+    private Games game;
+}
